4364d5c5c865585346df0e3def6325e101a1eac1
BJ Hargrave <hargrave@us.ibm.com>
2021-07-19 13:03:24 -0400

    example: Bnd Workspace based version of the example
    Signed-off-by: BJ Hargrave <hargrave@us.ibm.com>

---
+++ examples/osgi-test-example-bndworkspace/org.osgi.test.example.player.impl/test/org/osgi/test/example/player/impl/OpenBoxTest.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *******************************************************************************/
+
+package org.osgi.test.example.player.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+import org.junit.jupiter.api.Test;
+import org.osgi.test.example.api.Ball;
+
+public class OpenBoxTest {
+
+	@Test
+	void myTest() {
+		PlayerImpl p = new PlayerImpl();
+		p.ball = mock(Ball.class);
+		assertThat(p.getBall()).isNotNull();
+		verifyNoInteractions(p.getBall());
+		p.kickBall();
+		verify(p.getBall()).kick();
+	}
+
+}
